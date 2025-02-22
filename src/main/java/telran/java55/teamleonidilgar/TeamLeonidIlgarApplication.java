c1ab4bcb74fb97ef53a73cb93bd2874c9502bebd
Leonid Lovsky <leonid.lovsky@gmail.com>
2025-02-22 08:12:08 +0200

    Initial commit

---
+++ src/main/java/telran/java55/teamleonidilgar/TeamLeonidIlgarApplication.java
@@ -0,0 +1,12 @@
+package telran.java55.teamleonidilgar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamLeonidIlgarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamLeonidIlgarApplication.class, args);
+    }
+}
---
+++ src/test/java/telran/java55/teamleonidilgar/TeamLeonidIlgarApplicationTests.java
@@ -0,0 +1,12 @@
+package telran.java55.teamleonidilgar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamLeonidIlgarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
